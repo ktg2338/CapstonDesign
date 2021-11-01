9cd5c37826e248ea9f0bac45ebc4985b5d9aee17
ForteEscape <sehun5515@naver.com>
2021-11-01 13:52:09 +0900

    add basic django code

---
+++ capstone_project/main/static/js/index.js
@@ -0,0 +1,2 @@
+var inputObj = document.getElementById("id_coName");
+var inputValue = inputObj.options[inputObj.selectedIndex].value
--- capstone_project/main/views.py
+++ capstone_project/main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    if request.method == 'POST':
+        return render(request, 'capstone_project/index.html', {'POST': 'this is post method'})
+    else:
+        return render(request, 'capstone_project/index.html', {'GET': 'this is get method'})
